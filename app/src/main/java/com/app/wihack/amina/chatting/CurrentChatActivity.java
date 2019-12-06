e044eec4bc218525865ea396e191c2243d9704e9
Moham <m7md972013@gmail.com>
2019-12-06 23:01:16 +0200

    add current chat screen with row

---
+++ app/src/main/java/com/app/wihack/amina/chatting/CurrentChatActivity.java
@@ -0,0 +1,17 @@
+package com.app.wihack.amina.chatting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.app.wihack.amina.R;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_current_chat)
+public class CurrentChatActivity extends AppCompatActivity {
+
+    @AfterViews
+    public void after() {
+
+    }
+}
